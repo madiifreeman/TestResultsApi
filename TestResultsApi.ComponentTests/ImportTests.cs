cb9c6adde35e82dafdfb46c13f19771fd0142c71
madiifreeman <madisynlfreeman@outlook.com>
2025-05-05 07:46:21 +1000

    Implement required fields validation

--- TestResultsApi.ComponentTests/ImportTests.cs
+++ TestResultsApi.ComponentTests/ImportTests.cs
@@ -31,14 +31,49 @@ public class ImportTests
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
     
-    [Fact]
-    public async Task PostImport_WithInvalidXml_Returns400()
+    [Theory]
+    [InlineData("""
+                    <mcq-test-results>
+                        <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                            <student-number>521585128</student-number>
+                            <summary-marks available="20" obtained="13" />
+                        </mcq-test-result>
+                        <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                            <first-name>Jane</first-name>
+                            <last-name>Austen</last-name>
+                            <student-number>521585128</student-number>
+                            <test-id>1234</test-id>
+                            <summary-marks available="20" obtained="13" />
+                        </mcq-test-result>
+                    </mcq-test-results>
+                """)]
+    [InlineData("""
+                    <mcq-test-results>
+                        <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                            <test-id>1234</test-id>
+                            <summary-marks available="20" obtained="13" />
+                        </mcq-test-result>
+                    </mcq-test-results>
+                """)]
+    [InlineData("""
+                    <mcq-test-results>
+                        <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                            <test-id>1234</test-id>
+                            <student-number>521585128</student-number>
+                        </mcq-test-result>
+                    </mcq-test-results>
+                """)]
+    [InlineData("""
+                    <mcq-test-results>
+                        <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+                            <test-id>1234</test-id>
+                            <student-number>521585128</student-number>
+                            <summary-marks obtained="13" />
+                        </mcq-test-result>
+                    </mcq-test-results>
+                """)]
+    public async Task PostImport_WithMissingRequiredFields_Returns400(string invalidXml)
     {
-        var invalidXml = """
-                      <mcq-test-result>
-                      </mcq-test-result>
-                  """;
-
         var content = new StringContent(invalidXml, Encoding.UTF8, "text/xml+markr");
         var response = await _client.PostAsync("/import", content);
 
--- src/Models/McqTestResult.cs
+++ src/Models/McqTestResult.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TestResultsApi.Models;
 
 using System;
@@ -5,6 +7,7 @@ using System.Xml.Serialization;
 
 public class McqTestResult
 {
+    [Required]
     [XmlAttribute("scanned-on")]
     public string ScannedOnRaw { get; set; }
 
@@ -16,22 +19,27 @@ public class McqTestResult
 
     [XmlElement("last-name")]
     public string LastName { get; set; }
-
+    
+    [Required]
     [XmlElement("student-number")]
     public string StudentNumber { get; set; }
-
+    
+    [Required]
     [XmlElement("test-id")]
     public string TestId { get; set; }
 
+    [Required]
     [XmlElement("summary-marks")] 
     public SummaryMarks SummaryMarks { get; set; }
 }
 
 public class SummaryMarks
 {
+    [Required]
     [XmlAttribute("available")]
     public int Available { get; set; }
 
+    [Required]
     [XmlAttribute("obtained")]
     public int Obtained { get; set; }
     
